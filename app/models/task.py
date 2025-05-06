9e01b9abc661db481e7f82b71fd8abfae0f6c256
KateM <ekaterinamarantidi@gmail.com>
2025-05-05 18:44:34 -0700

    Wave_1 tests passed

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,36 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime
+from datetime import datetime
 from ..db import db
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[datetime | None] = mapped_column(DateTime, nullable=True)
+
+    @classmethod
+    def from_dict(cls, task_data):
+        return cls(
+            title=task_data["title"], 
+            description=task_data["description"],
+            completed_at=task_data.get("completed_at")
+        )
+
+        # new_task = Task(title=task_data["title"], 
+        # description=task_data["description"],
+        # completed_at=task_data["completed_at"])
+        # return new_task
+    
+    def to_dict(self, include_completed_at=False):
+        task_as_dict = {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": bool(self.completed_at)
+    }
+
+        if include_completed_at:
+            task_as_dict["completed_at"] = self.completed_at
+
+        return task_as_dict
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,78 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response, request, Response
+from app.models.task import Task
+from ..db import db
+from .route_utilities import validate_model
+
+bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+#Wave 1
+@bp.post("")
+def create_task():
+    request_body = request.get_json()
+    
+    try:
+        new_task = Task.from_dict(request_body)
+
+    except KeyError as error:
+        if "description" not in request_body  or "title" not in request_body:
+            response = {"details": "Invalid data"}
+        else:
+            response = {"details": f"Invalid request: missing {error.args[0]}"}
+
+        abort(make_response(response, 400))
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return {"task": new_task.to_dict()}, 201
+
+#Wave 3 and Wave 5
+@bp.get("")
+def get_all_tasks():
+    query = db.select(Task)
+    description_param = request.args.get("description")
+    if description_param:
+        query = query.where(Task.description.ilike(f"%{description_param}%"))
+        
+    completed_at_param = request.args.get("completed_at")
+    if completed_at_param:
+        query = query.where(Task.completed_at.like=={completed_at_param})
+        
+    query = query.order_by(Task.id)
+    tasks = db.session.scalars(query)
+
+    tasks_response = []
+    for task in tasks:
+        tasks_response.append(task.to_dict())
+    
+    return tasks_response
+
+#Wave 1
+@bp.get("/<task_id>")
+def get_one_task(task_id):
+    task = validate_model(Task, task_id)
+    return {"task": task.to_dict()}
+
+#Wave 4
+@bp.put("/<task_id>")
+def update_one_task(task_id):
+    task = validate_model(Task, task_id)
+    request_body = request.get_json()
+    
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+    task.completed_at = request_body.get("completed_at")
+    db.session.commit()
+    # needs fixing
+    return Response(status=204, mimetype="application/json")
+
+#Wave 4
+@bp.delete("/<task_id>")
+def delete_one_task(task_id):
+    task = validate_model(Task, task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+    
