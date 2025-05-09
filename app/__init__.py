0268b8d2fbd09184730fa650bfcc8868478b612c
KateM <ekaterinamarantidi@gmail.com>
2025-05-09 10:02:53 -0700

    added 'load_dotenv()'

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from .models import task, goal
 from .routes.task_routes import bp as tasks_bp
 from .routes.goal_routes import bp as goals_bp
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def create_app(config=None):
     app = Flask(__name__)
